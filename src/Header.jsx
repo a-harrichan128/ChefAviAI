467e26376f8e7065ced733e8960c739435e30a45
Avinash Harrichan <a_harrichan@live.ca>
2025-10-28 14:47:21 -0400

    updated header so that img will show with correct path on all deployments

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 export default function Header() {
   return (
     <header className="header-title">
-      <img src="/pic1.jpg" alt="headshot" />
+      <img src="ChefAviAI/pic1.jpg" alt="headshot" />
       <h1>Chef Avi powered by AI</h1>
     </header>
   );
