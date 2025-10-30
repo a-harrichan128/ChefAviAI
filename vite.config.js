9c2ceb3b77dc13e3363ccc083ac7560bffadc43a
Avinash Harrichan <a_harrichan@live.ca>
2025-10-30 11:57:26 -0400

    updated a few files with comments to remember what i did

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ChefAviAI",
+  base: "/",
 });
+
+//changing base changes behaviour of relative paths, be mindful of how you path to assets
